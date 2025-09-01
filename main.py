bfd5d1cb7979d566c075a8a21f114a192c000bdd
Timur <uxxxsr@gmail.com>
2025-09-01 21:09:47 +0500

    add1

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from tabulate import tabulate
 import csv
-import time
-#открываем файл
-with open('bd.csv', encoding='utf-8') as bd:
-    #читаем построчно
-    read = csv.reader(bd)
-    #выводим циклом построчечно
-    for row in read:
-        print(row)
-        time.sleep(0.1)
-        
+def print_menu_from_csv(filename):
+    with open('bd.csv', 'r', encoding='utf-8') as file:
+        # Читаем данные из файла
+        csv_reader = csv.DictReader(file)
+        data = list(csv_reader)
+        print(tabulate(data, headers="keys", tablefmt="grid"))
+print_menu_from_csv('bd.csv')
